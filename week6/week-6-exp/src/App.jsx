26fade62488a64fb6732ab54f2b3ffb592f98b61
DhanunjayJ <jagudhanunjai@gmail.com>
2024-10-08 19:08:28 +0000

    react returns

---
+++ week6/week-6-exp/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+        <Header title="dj"></Header>
+        <Header title="dj1"></Header>
+    </>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
