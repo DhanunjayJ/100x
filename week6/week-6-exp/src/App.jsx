16c6b85fd094c827144e6d34584809659fb6563a
DhanunjayJ <jagudhanunjai@gmail.com>
2024-10-08 19:19:48 +0000

    rerender 1exp

--- week6/week-6-exp/src/App.jsx
+++ week6/week-6-exp/src/App.jsx
@@ -1,17 +1,27 @@
-function App() {
+import React,{Fragment} from "react";
+import {useState} from "react"
 
+
+function App() {
+  const [name,setName] = useState("dj");
+
+  function updateTitle(){
+    setName("my name is "+Math.random());
+  }
   return (
-    <>
-        <Header title="dj"></Header>
+    <Fragment>
+        <button onClick={updateTitle}>Update the title</button>
+        <Header title={name}></Header>
         <Header title="dj1"></Header>
-    </>
+     </Fragment>
   )
 }
-
 function Header({title}){
   return <div>
     {title}
   </div>
 }
+
+
 
 export default App
